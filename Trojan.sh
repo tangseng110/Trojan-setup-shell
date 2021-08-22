c138375868e7274c026ebb701dab742d7737c0bf
tangseng110 <44749474+tangseng110@users.noreply.github.com>
2021-08-22 22:53:15 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -155,7 +155,6 @@ EOF
 	#申请https证书
 	mkdir /usr/src/trojan-cert
 	curl https://get.acme.sh | sh
-	~/.acme.sh/acme.sh  --set-default-ca  --server zerossl
 	~/.acme.sh/acme.sh  --register-account  --server zerossl \
         --eab-kid  BUynZMQMPb7DxpvjY1LZYA \
         --eab-hmac-key  8oK3X_XELrTHHFwNPnjasVE94-V1rmZxEywkkzIWxmf0gD5y15wo-hPw6aRImamihr9mTc-_J2pY7SMGVKhgWQ
