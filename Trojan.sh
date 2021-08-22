12047d08c6387ad41d9f4ff160838365c3b97976
tangseng110 <44749474+tangseng110@users.noreply.github.com>
2021-08-22 22:49:05 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -156,7 +156,9 @@ EOF
 	mkdir /usr/src/trojan-cert
 	curl https://get.acme.sh | sh
 	~/.acme.sh/acme.sh  --set-default-ca  --server zerossl
-	~/.acme.sh/acme.sh  --register-account  -m myemail@example.com --server zerossl
+	~/.acme.sh/acme.sh  --register-account  --server zerossl \
+        --eab-kid  BUynZMQMPb7DxpvjY1LZYA \
+        --eab-hmac-key  8oK3X_XELrTHHFwNPnjasVE94-V1rmZxEywkkzIWxmf0gD5y15wo-hPw6aRImamihr9mTc-_J2pY7SMGVKhgWQ
 	~/.acme.sh/acme.sh  --set-default-ca  --server zerossl
 	~/.acme.sh/acme.sh  --server zerossl  --issue  -d $your_domain  --webroot /usr/share/nginx/html/ 
     	~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
